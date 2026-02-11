11c60d7705f41498fd8e46e8b11de05efeed1a6e
Andy Wang <fangwanggo@outlook.com>
2026-02-11 12:50:50 -0800

    changed path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': '/src',
